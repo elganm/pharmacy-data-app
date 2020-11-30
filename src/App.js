27d69081bb2398d2226a01df918bc7f061281d4d
Elgan Mathias <e_mathias@hotmail.co.uk>
2020-11-30 22:37:04 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data';
 
-function App() {
+function App(props) {
+
+  //filer contractor addresses with postcode that user will search for
+  const dataItemCheck = data["Contractor Addresses"].filter((item) => (item["Post Code"] === "NP44 1DZ"));
+  //map Contractor Addresses for account number to cross-reference with the number of prescription items later
+  const dataItem = data["Contractor Addresses"].map(item =>  item["Account Number"]);
+  //get trading name for the pharmacy the user searched for
+  const dataItemName = dataItemCheck.map(item => item["Trading Name"]);
+ 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>
+          Postcode:
+        <input type="text" name="postcode"/>
+        </label>
+        <input type="submit" value="submit" />
+      </form>
+      
+      <h1>{dataItem}</h1>
+  <h2>{dataItemName}</h2>
     </div>
   );
 }
